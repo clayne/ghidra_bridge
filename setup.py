5eac220cf8e6a51ca0988d5983303f74f6420402
justfoxing <justfoxingprojects@gmail.com>
2023-01-30 14:30:56 +1100

    Fix up versioning to work for GHA

--- setup.py
+++ setup.py
@@ -4,9 +4,18 @@ import subprocess
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# determine the version, then write it out into the bridge.py file
+version = (
+    subprocess.check_output("git describe --tags", shell=True).decode("utf-8").strip()
+)
+# check if this is a non-tag release and remark it as a dev release
+if "-" in version:
+    ver, commits, hash = version.split("-")
+    version = ver + ".dev" + commits
+
 setuptools.setup(
     name="ghidra_bridge",
-    version=subprocess.check_output("git describe", shell=True).decode("utf-8").strip(),
+    version=version,
     author="justfoxing",
     author_email="justfoxingprojects@gmail.com",
     description="RPC bridge from Python to Ghidra Jython",
